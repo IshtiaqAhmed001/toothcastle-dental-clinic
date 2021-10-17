7fc47e49caf167e815431aa186cce94c7f84708e
IshtiaqAhmed001 <ishtiaq.wasi@gmail.com>
2021-10-18 00:51:06 +0600

    first commit

---
+++ src/Firebase/config.firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCefwGq-RDXuWJAM-KKFfRaq7s8K40K_xE",
+    authDomain: "tooth-castle-dental-clinic.firebaseapp.com",
+    projectId: "tooth-castle-dental-clinic",
+    storageBucket: "tooth-castle-dental-clinic.appspot.com",
+    messagingSenderId: "10004225848",
+    appId: "1:10004225848:web:d008e3b67e90e1f8eeebcd"
+};
+
+export default firebaseConfig;
