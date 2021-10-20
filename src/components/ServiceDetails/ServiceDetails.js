135fd46713e599e3e0296809059c3ad3834bc124
IshtiaqAhmed001 <ishtiaq.wasi@gmail.com>
2021-10-20 13:13:32 +0600

    individual details works

--- src/components/ServiceDetails/ServiceDetails.js
+++ src/components/ServiceDetails/ServiceDetails.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import { useParams } from 'react-router';
+import useServices from '../../hooks/useServices';
 
 const ServiceDetails = () => {
-    const { serviceNo } = useParams();
+    let { serviceNo } = useParams();
+    const { services } = useServices();
+    console.log(services);
+    serviceNo = parseInt(serviceNo);
+    console.log(serviceNo)
+
+    const showDetails = services.filter(service => parseInt(service.id) === serviceNo);
+    console.log(showDetails);
 
     return (
         <div className="my-5">
             <h1 className="my-5">THis is service no: {serviceNo}</h1>
+            {
+                <div>
+                    <h2>Name: {showDetails[0].name}</h2>
+                    <img src={showDetails[0].img} alt="" />
+                </div>
 
+            }
         </div >
     );
 };
